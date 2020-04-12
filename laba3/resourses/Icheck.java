d687ce3d836f3b2d3d97814599058e49e4f102f6
GGOShinoAsada <63353660+GGOShinoAsada@users.noreply.github.com>
2020-04-13 01:47:57 +0300

    Add files via upload

---
+++ laba3/resourses/Icheck.java
@@ -0,0 +1,5 @@
+package com.company.classes;
+public interface Icheck {
+boolean check(String s);
+boolean check();
+}
---
+++ laba3/resourses/MaxCheck.java
@@ -0,0 +1,23 @@
+package com.company.resourses;
+public class MaxCheck implements Icheck{ public static String S;
+public MaxCheck(String str){
+S=str;
+}
+//get string default public boolean check(){
+return method(S);
+}
+//get override string
+public boolean check(String s){
+return method(s);
+}
+boolean method(String str){
+boolean b;
+b=false;
+a1:for (int i=0;i<str.length()-1;i++){ for (int j=i+1;j<str.length();j++){ if (str.charAt(i)<=str.charAt(j)){ b=true;
+}else{ b=false; break al;
+}
+}
+}
+return b;
+}
+}
